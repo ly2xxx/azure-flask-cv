e978b3c9c21ccf53a3d56566866b2b94bb90b318
ly2xxx <ly2xxx@hotmail.com>
2023-12-04 00:03:29 +0000

    intial commit

--- app.py
+++ app.py
@@ -1,32 +1,53 @@
+from flask import Flask, render_template, request
 import os
-
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+import pinecone
+from sentence_transformers import SentenceTransformer
 
 app = Flask(__name__)
 
+def get_query_results(question):
+    # Set Pinecone API and environment
+    pinecone_api = os.environ["PINECONE_API"]
+    pinecone_env = os.environ["PINECONE_ENV"]
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    # Initialize Pinecone
+    pinecone.init(api_key=pinecone_api, environment=pinecone_env)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    # Set Pinecone index
+    index_name = "pdfsearchdx"
+    index = pinecone.Index(index_name)
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+    # Set Hugging Face Hub API token
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = str(os.getenv("HUGGINGFACEHUB_API_TOKEN"))
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+    # Set query questions
+    query_questions = [question, ]
+
+    # Initialize Sentence Transformer model
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    # Encode query questions
+    query_vectors = [model.encode(str(question)).tolist() for question in query_questions]
+
+    # Perform Pinecone query
+    query_results = index.query(vector=query_vectors, top_k=2)
+
+    return query_results
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    submitted_question = "What is my name?"
+    query_results = "No results"
+
+    if request.method == 'POST':
+        submitted_question = request.form['question']
+
+        # Check if the submitted question is not empty
+        if submitted_question:
+            query_results = get_query_results(submitted_question)
+
+    return render_template('index.html', query_results=str(query_results), submitted_question=submitted_question)
 
 
 if __name__ == '__main__':
-   app.run()
+    app.run(debug=True)
